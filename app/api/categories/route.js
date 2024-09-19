c2a4775072d91ae7116670a87293d51c70df35f3
Pasanmahee <pasanmahee.roo@gmail.com>
2024-09-19 11:37:02 +0530

    docker file add

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
 const client = new MongoClient(uri);
 
 function setCORSHeaders() {
