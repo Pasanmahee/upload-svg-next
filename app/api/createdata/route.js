371c024852bfe6b49196d92e113a599d9189857f
pasanmahee <pasanmahee.roo@gmail.com>
2024-12-09 11:43:30 +0530

    projection error fixed

--- app/api/createdata/route.js
+++ app/api/createdata/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { MongoClient, ObjectId } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
 const client = new MongoClient(uri);
@@ -35,23 +35,20 @@ export async function GET(req) {
     const database = client.db('svgfacetpaintbynumber');
     const collection = database.collection('svgdata');
 
-    // Simplify the query to match documents where userId equals the provided userId
     const query = { userId };
 
-    // Query to retrieve only the relevant fields with pagination
     const data = await collection.find(query, {
       projection: {
         _id: 1,
-        userId,
+        userId: 1,
         pngData: 1,
-        date: 1,
+        date: 1
       },
     })
       .skip(skip)
       .limit(limit)
       .toArray();
 
-    // Retrieve the total count of documents that match the query for pagination metadata
     const total = await collection.countDocuments(query);
     const totalPages = Math.ceil(total / limit);
 
