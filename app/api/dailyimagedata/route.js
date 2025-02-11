d9dcc1430276760d557dae711dc4f9af96f11d17
pasanmahee <pasanmahee.roo@gmail.com>
2025-02-11 13:06:52 +0530

    sort removed

--- app/api/dailyimagedata/route.js
+++ app/api/dailyimagedata/route.js
@@ -68,7 +68,6 @@ export async function GET(req) {
     // 7. Retrieve data with sorting, pagination, projection
     const data = await collection
       .find(query, { projection })
-      .sort({ date: -1 }) // Most recent first
       .skip(skip)
       .limit(limit)
       .toArray();
